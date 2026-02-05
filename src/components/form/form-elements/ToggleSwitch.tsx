04f076aefe1fb953739e48ca86eb1c994bf570ee
overflux <overflux.tech@gmail.com>
2026-02-05 15:50:23 +0530

    prop pass

--- src/components/form/form-elements/ToggleSwitch.tsx
+++ src/components/form/form-elements/ToggleSwitch.tsx
@@ -10,12 +10,12 @@ export default function ToggleSwitch() {
       <div className="flex gap-4">
         <Switch
           label="Default"
-          defaultChecked={true}
+          checked={true}
           onChange={handleSwitchChange}
         />
         <Switch
           label="Checked"
-          defaultChecked={true}
+          checked={true}
           onChange={handleSwitchChange}
         />
         <Switch label="Disabled" disabled={true} />
@@ -23,13 +23,13 @@ export default function ToggleSwitch() {
       <div className="flex gap-4">
         <Switch
           label="Default"
-          defaultChecked={true}
+          checked={true}
           onChange={handleSwitchChange}
           color="gray"
         />
         <Switch
           label="Checked"
-          defaultChecked={true}
+          checked={true}
           onChange={handleSwitchChange}
           color="gray"
         />
